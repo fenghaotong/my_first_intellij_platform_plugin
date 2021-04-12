bd98d157bde4955c711858cf18af21c3670199e3
GitHub Action <action@github.com>
2021-04-12 10:06:51 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "my_first_intellij_platform_plugin"
---
+++ src/main/kotlin/com/github/fenghaotong/myfirstintellijplatformplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.fenghaotong.myfirstintellijplatformplugin.services
+
+import com.github.fenghaotong.myfirstintellijplatformplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
